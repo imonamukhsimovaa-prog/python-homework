b3e16dce6583360cc85d8b32d09a55744621435c
imonamukhsimovaa-prog <imonamukhsimovaa@gmail.com>
2025-11-09 16:07:36 +0500

    Create task1.py

---
+++ lesson-1/homework/task1.py
@@ -0,0 +1,27 @@
+side = float(input("Enter the side of the square "))
+perimeter = 4 * side
+area = side * side
+print("Perimeter", perimeter)
+print("Area", area)
+
+
+import math
+diameter = float(input("Enter the diameter of the circle "))
+length = math.pi * diameter
+print("Circumference", length)
+
+
+a = float(input("Enter number a "))
+b = float(input("Enter number b "))
+mean = (a + b) / 2
+print("Mean value", mean)
+
+sum_ab = a + b
+product = a * b
+square_a = a ** 2
+square_b = b ** 2
+
+print("Sum", sum_ab)
+print("Product", product)
+print("a squared", square_a)
+print("b squared", square_b)
